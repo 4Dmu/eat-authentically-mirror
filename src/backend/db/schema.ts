65b698be6228231e2a92c1c0d292e4557b1f970f
4Dmu <xsmh7bhy@duck.com>
2025-08-28 13:15:15 +0100

    fix: Remove invalid listings table column primaryImageIdx and unused tables products and uploadhistory.

--- src/backend/db/schema.ts
+++ src/backend/db/schema.ts
@@ -66,12 +66,10 @@ export const listings = sqliteTable("listings", {
   type: text({ enum: LISTING_TYPES }).notNull(),
   claimed: integer({ mode: "boolean" }).notNull(),
   verified: integer({ mode: "boolean" }).notNull(),
-
   about: text(),
   images: text({ mode: "json" })
     .$type<{ primaryImgId: string | null; items: ImageData[] }>()
     .notNull(),
-  primaryImageIdx: integer(),
   pendingImages: text({ mode: "json" }).$type<
     { id: string; isPrimary: boolean }[]
   >(),
@@ -88,7 +86,6 @@ export const listings = sqliteTable("listings", {
 });
 
 export const listingsRelations = relations(listings, ({ many, one }) => ({
-  products: many(products),
   certificationsToListings: many(certificationsToListings),
   oranization: one(organizations, {
     fields: [listings.organizationId],
@@ -120,16 +117,6 @@ export const organizations = sqliteTable("organizations", {
   updatedAt: integer({ mode: "timestamp" }).notNull(),
 });
 
-export const uploadHistory = sqliteTable("upload_history", {
-  organizationId: text()
-    .primaryKey()
-    .references(() => organizations.id),
-  imageUploadAttempts: text({ mode: "json" })
-    .$type<ImageUploadAttempt[]>()
-    .notNull(),
-  imageUploads: text({ mode: "json" }).$type<ImageUpload[]>().notNull(),
-});
-
 export const certificationsToListings = sqliteTable(
   "certifications_to_listings",
   {
@@ -156,31 +143,5 @@ export const certificationsToListingsRelations = relations(
     }),
   })
 );
-
-export const products = sqliteTable("products", {
-  id: text().primaryKey(),
-  companyProfileId: text()
-    .notNull()
-    .references(() => listings.id),
-  name: text().notNull(),
-});
-
-export const productsRelations = relations(products, ({ one }) => ({
-  bussinessProfile: one(listings, {
-    fields: [products.companyProfileId],
-    references: [listings.id],
-  }),
-}));
-
-export const organizationsRelations = relations(organizations, ({ one }) => ({
-  uploadHistory: one(uploadHistory),
-}));
-
-export const uploadHistoryRelations = relations(uploadHistory, ({ one }) => ({
-  organization: one(organizations, {
-    fields: [uploadHistory.organizationId],
-    references: [organizations.id],
-  }),
-}));
 
 export type Certification = typeof certifications.$inferSelect;
