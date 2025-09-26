f2ff9d74c841aa43d6c6dd981d641c5e45c8f1d5
4Dmu <xsmh7bhy@duck.com>
2025-09-26 06:49:06 +0100

    fix: Use classname prop in back button component

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,12 +1,13 @@
 import React, { ComponentProps } from "react";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export function BackButton(
   props: Omit<ComponentProps<typeof Link>, "children"> & { text: string }
 ) {
   return (
-    <Link {...props} className="flex gap-2 items-center">
+    <Link {...props} className={cn("flex gap-2 items-center", props.className)}>
       <ArrowLeft size={20} />
       <span className="font-bold text-sm">{props.text}</span>
     </Link>
