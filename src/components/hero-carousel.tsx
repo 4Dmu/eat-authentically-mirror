a6ca5c0c02436eff33b0a7c1f02986a8fbe9024c
4Dmu <rl7uaq1g@duck.com>
2025-10-20 16:40:31 +0100

    feat: Replace hero carousel with single search box

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -1,58 +1,14 @@
-import {
-  Carousel,
-  CarouselApi,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
 import { Input } from "./ui/input";
-import { CompassIcon, MapPin } from "lucide-react";
-import { PRODUCER_TYPES } from "@/backend/validators/producers";
-import {
-  ChangeEvent,
-  KeyboardEvent,
-  useCallback,
-  useEffect,
-  useState,
-} from "react";
+import { CompassIcon } from "lucide-react";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
 import { showFilterMenuAtom, useHomePageStore } from "@/stores";
 import Image from "next/image";
-import { match } from "ts-pattern";
 import { Button } from "./ui/button";
 import { useSetAtom } from "jotai";
-
-const slides = [
-  {
-    id: "food",
-    imageUrl: "/hero/FRF_Food.png",
-    filter: undefined,
-    title: "Eat Authentically",
-  },
-  {
-    id: "farm",
-    imageUrl: "/hero/FRF_Farm_Default.jpg",
-    filter: "farm",
-    title: "Authentic Farms",
-  },
-  {
-    id: "ranch",
-    imageUrl: "/hero/FRF_Ranch_Default.jpg",
-    filter: "ranch",
-    title: "Authentic Ranches",
-  },
-  {
-    id: "eatery",
-    imageUrl: "/hero/FRF_Eaterie_Default.jpg",
-    filter: "eatery",
-    title: "Authentic Eateries",
-  },
-];
 
 function SearchBox({ title }: { title: string }) {
   const { setQuery, query } = useHomePageStore();
   const [value, setValue] = useState(query ?? "");
-  const setShowFilterMenu = useSetAtom(showFilterMenuAtom);
 
   function updateQuery(e: ChangeEvent<HTMLInputElement>) {
     setValue(e.currentTarget.value);
@@ -99,70 +55,82 @@ function SearchBox({ title }: { title: string }) {
 }
 
 export function HeroCarousel() {
-  const [api, setApi] = useState<CarouselApi>();
+  // const [api, setApi] = useState<CarouselApi>();
   const { setTypeFilter, typeFilter } = useHomePageStore();
 
-  const handler = useCallback(() => {
-    if (!api) return;
-    const index = api.selectedScrollSnap();
-    const type = index == 0 ? undefined : PRODUCER_TYPES[index - 1];
-    setTypeFilter(type);
-  }, [api, setTypeFilter]);
+  // const handler = useCallback(() => {
+  //   if (!api) return;
+  //   const index = api.selectedScrollSnap();
+  //   const type = index == 0 ? undefined : PRODUCER_TYPES[index - 1];
+  //   setTypeFilter(type);
+  // }, [api, setTypeFilter]);
 
-  useEffect(() => {
-    if (!api) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!api) {
+  //     return;
+  //   }
 
-    api.on("select", handler);
-    return () => {
-      api.off("select", handler);
-    };
-  }, [api, handler]);
+  //   api.on("select", handler);
+  //   return () => {
+  //     api.off("select", handler);
+  //   };
+  // }, [api, handler]);
 
-  useEffect(() => {
-    if (!api) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!api) {
+  //     return;
+  //   }
 
-    const currentIndex = api.selectedScrollSnap();
+  //   const currentIndex = api.selectedScrollSnap();
 
-    const indexForTypeFilter = match(typeFilter)
-      .with(undefined, () => 0)
-      .with("farm", () => 1)
-      .with("ranch", () => 2)
-      .with("eatery", () => 3)
-      .exhaustive();
+  //   const indexForTypeFilter = match(typeFilter)
+  //     .with(undefined, () => 0)
+  //     .with("farm", () => 1)
+  //     .with("ranch", () => 2)
+  //     .with("eatery", () => 3)
+  //     .exhaustive();
 
-    if (currentIndex !== indexForTypeFilter) {
-      api.scrollTo(indexForTypeFilter, true);
-    }
-  }, [typeFilter, api]);
+  //   if (currentIndex !== indexForTypeFilter) {
+  //     api.scrollTo(indexForTypeFilter, true);
+  //   }
+  // }, [typeFilter, api]);
 
   return (
-    <Carousel
-      setApi={setApi}
-      className="h-[50vh] flex w-full"
-      opts={{ loop: true }}
-    >
-      <CarouselContent className="h-full w-full">
-        {slides.map((slide) => (
-          <CarouselItem className="h-full w-full relative" key={slide.id}>
-            <Image
-              priority={slide.id === "food"}
-              width={1920}
-              height={1080}
-              alt={slide.title}
-              src={slide.imageUrl}
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute top-0 left-0 w-full bg-black/10 h-full" />
-            <SearchBox title={slide.title} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselNext className="max-md:top-3/4 max-md:translate-y-10" />
-      <CarouselPrevious className="max-md:top-3/4 max-md:translate-y-10" />
-    </Carousel>
+    // <Carousel
+    //   setApi={setApi}
+    //   className="h-[50vh] flex w-full"
+    //   opts={{ loop: true }}
+    // >
+    //   <CarouselContent className="h-full w-full">
+    //     {slides.map((slide) => (
+    //       <CarouselItem className="h-full w-full relative" key={slide.id}>
+    //         <Image
+    //           priority={slide.id === "food"}
+    //           width={1920}
+    //           height={1080}
+    //           alt={slide.title}
+    //           src={slide.imageUrl}
+    //           className="w-full h-full object-cover"
+    //         />
+    //         <div className="absolute top-0 left-0 w-full bg-black/10 h-full" />
+    //         <SearchBox title={slide.title} />
+    //       </CarouselItem>
+    //     ))}
+    //   </CarouselContent>
+    //   <CarouselNext className="max-md:top-3/4 max-md:translate-y-10" />
+    //   <CarouselPrevious className="max-md:top-3/4 max-md:translate-y-10" />
+    // </Carousel>
+    <div className="h-full w-full relative">
+      <Image
+        priority={true}
+        width={1920}
+        height={1080}
+        alt={""}
+        src={"/hero/FRF_Food.png"}
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute top-0 left-0 w-full bg-black/10 h-full" />
+      <SearchBox title={"Eat Authentically"} />
+    </div>
   );
 }
