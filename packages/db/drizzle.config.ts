8082f0c17630e9b6d9eeacefbf1f4d5d059b013d
4Dmu <xsmh7bhy@duck.com>
2025-11-05 12:04:11 +0000

    feat: Move migration files to db package

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,7 +3,7 @@ import { env } from "./src/env";
 
 export default defineConfig({
   dialect: "turso", // 'mysql' | 'sqlite' | 'turso'
-  schema: "./src/backend/db/schema.ts",
+  schema: "./src/schema.ts",
   dbCredentials: {
     url: env.TURSO_DB_URL,
     authToken: env.TURSO_DB_TOKEN,
